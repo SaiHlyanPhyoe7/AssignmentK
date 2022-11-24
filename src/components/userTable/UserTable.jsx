744b862f439e2f9d917be3c1d9e5c65ad3fe945f
SaiHlyanPhyoe7 <saihlyan.phyoe7@gmail.com>
2022-11-25 02:47:43 +0630

    pagination done

--- src/components/userTable/UserTable.jsx
+++ src/components/userTable/UserTable.jsx
@@ -1,18 +1,39 @@
-import React, { useEffect, useState } from 'react';
-import {useCollection} from '../../hooks/useCollection'
+import React, { useEffect, useState } from "react";
+import { useCollection } from "../../hooks/useCollection";
 
-
-import {db} from "../../firebase/config"
-import {collection,getDocs} from "firebase/firestore"
-import ChildUserTable from './tableChild/ChildUserTable';
+import { db } from "../../firebase/config";
+import { collection, getDocs } from "firebase/firestore";
+import ChildUserTable from "./tableChild/ChildUserTable";
 
 const UserTable = () => {
-    const {documents: formValues} = useCollection('formData')
-    console.log(formValues)
-    return (
-        <ChildUserTable formValues={formValues}/>
+  const { documents: formValues } = useCollection("formData");
+  console.log(formValues);
+  const [itemOffset, setItemOffset] = useState(0);
+  const itemsPerPage = 5;
+  const length = Math.ceil(formValues?.length / 5);
+  const endOffset = itemOffset + itemsPerPage;
+  console.log(`Loading items from ${itemOffset} to ${endOffset}`);
+  const currentItems = formValues?.slice(itemOffset, endOffset);
+  console.log(currentItems);
+  const pageCount = Math.ceil(formValues?.length / itemsPerPage);
+
+  const handlePageClick = (event) => {
+    const newOffset = (event.selected * itemsPerPage) % formValues?.length;
+    console.log(
+      `User requested page number ${event.selected}, which is offset ${newOffset}`
     );
+    setItemOffset(newOffset);
+  };
+  return (
+    <>
+      <ChildUserTable
+        formValues={currentItems}
+        pageCount={pageCount}
+        handlePageClick={handlePageClick}
+        length={length}
+      />
+    </>
+  );
 };
 
 export default UserTable;
-
--- src/components/userTable/tableChild/ChildUserTable.jsx
+++ src/components/userTable/tableChild/ChildUserTable.jsx
@@ -1,85 +1,137 @@
-import {db} from '../../../firebase/config'
-import {doc, deleteDoc} from "firebase/firestore"
-import HandleManipulation from '../handleManipulation/HandleManipulation'
+import { db } from "../../../firebase/config";
+import { doc, deleteDoc } from "firebase/firestore";
+import HandleManipulation from "../handleManipulation/HandleManipulation";
+import ReactPaginate from "react-paginate";
 
+const ChildUserTable = ({ formValues, handlePageClick, pageCount, length }) => {
+  const handleDelete = async (id) => {
+    const ref = doc(db, "formData", id);
+    await deleteDoc(ref);
+  };
 
-const ChildUserTable = ({formValues}) => {
-
-    const handleDelete = async (id)=>{
-        const ref = doc(db,'formData',id)
-        await deleteDoc(ref)
-    }
-    
   return (
-    <div className='main-box'>
-            <div className='real-box'>
-                <h1 className='box-title'>Customers</h1>
-                <form className='form-box'>
-                    <input type="text" className='form-input' placeholder='Start Date' />
-                    <input type="text" className='form-input ml' placeholder='End Date' />
-                    <input type="submit" className='submit-btn ml' />
-                </form>
-                <button className='export-btn'>Export CSV</button>
-                <div className='second-flex'>
-                        <div className='select-box'>
-                            <select name="" id="" className='select'>
-                                <option value="5">5</option>
-                            </select>
-                        <label>entries per page</label>
-                    </div>
-                        <input type="text" className='responsive_br' placeholder='Search...' />
-                    </div>
-                <table className='table'>
-                    <thead className='theader'>
-                        <tr>
-                            <td className='thead responsive_h' width='50px'>PHOTO</td>
-                            <td className='thead' width='200px'>NAME</td>
-                            <td className='thead' width='170px'>EMAIL</td>
-                            <td className='thead' width='120px'>PHONE</td>
-                            <td className='thead responsive-table' width='150px'>BIRTHDAY</td>
-                            <td className='thead responsive-table' width='100px'>GENDER</td>
-                            <td className='thead responsive-table' width='150px'>NRC</td>
-                            <td className='thead responsive-table' width='250px'>Created At</td>
-                            <td className='thead responsive_h' width='200px'>Actions</td>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {formValues &&
-                        formValues.map((data,index)=>{  
-                            return(
-                                <tr key={index} className='tdata'>
-                                    <td className='responsive_h' width='100px'>
-                                        <img style={{width:'50px',height:'50px',objectFit:"cover"}} 
-                                        src={data.photo} alt="" />
-                                    </td>
-                                    <td className='tdata'>{data.name}</td>
-                                    <td className='tdata' width='250px'>{data.email}</td>
-                                    <td className='tdata'>{data.phNumber}</td>
-                                    <td className='tdata responsive-table'>{data.birthday}</td>
-                                    <td className='tdata responsive-table'>{data.gender}</td>
-                                    <td className='tdata responsive-table'>{data.nrc_num}/{data.nrc_state}{data.nrc_national}/{data.nrc_numbers}</td>
-                                    <td className='tdata responsive-table'>{data.time} {data.hour} H</td>
-                                    <HandleManipulation data={data} handleDelete={handleDelete}/>
-                                </tr>
-                            )
-                        })
-                    }
-        
-                    </tbody>
-                </table>
-                <div className='second-flex'>
-                    <p className='except'>Showing 1 to 15 of 14 entries</p>
-                    <div className='btn-box'>
-                        <button className='active'>1</button>
-                        <button>2</button>
-                        <button>3</button>
-                        <button>4</button>
-                        <button>></button>
-                    </div>
-                </div>
+    <>
+      <div className="main-box">
+        <div className="real-box">
+          <h1 className="box-title">Customers</h1>
+          <form className="form-box">
+            <input
+              type="text"
+              className="form-input"
+              placeholder="Start Date"
+            />
+            <input
+              type="text"
+              className="form-input ml"
+              placeholder="End Date"
+            />
+            <input type="submit" className="submit-btn ml" />
+          </form>
+          <button className="export-btn">Export CSV</button>
+          <div className="second-flex">
+            <div className="select-box">
+              <select name="" id="" className="select">
+                <option value="5">5</option>
+              </select>
+              <label>entries per page</label>
             </div>
+            <input
+              type="text"
+              className="responsive_br"
+              placeholder="Search..."
+            />
+          </div>
+          <table className="table">
+            <thead className="theader">
+              <tr>
+                <td className="thead responsive_h_p" width="50px">
+                  PHOTO
+                </td>
+                <td className="thead" width="200px">
+                  NAME
+                </td>
+                <td className="thead" width="170px">
+                  EMAIL
+                </td>
+                <td className="thead" width="120px">
+                  PHONE
+                </td>
+                <td className="thead responsive_h" width="150px">
+                  BIRTHDAY
+                </td>
+                <td className="thead responsive-table" width="100px">
+                  GENDER
+                </td>
+                <td className="thead responsive-table" width="150px">
+                  NRC
+                </td>
+                <td className="thead responsive-table" width="250px">
+                  Created At
+                </td>
+                <td className="thead responsive_h" width="200px">
+                  Actions
+                </td>
+              </tr>
+            </thead>
+            <tbody>
+              {formValues &&
+                formValues.map((data, index) => {
+                  return (
+                    <tr key={index} className="tdata">
+                      <td className="responsive_h_p" width="100px">
+                        <img
+                          style={{
+                            width: "50px",
+                            height: "50px",
+                            objectFit: "cover",
+                          }}
+                          src={data.photo}
+                          alt=""
+                        />
+                      </td>
+                      <td className="tdata">{data.name}</td>
+                      <td className="tdata" width="250px">
+                        {data.email}
+                      </td>
+                      <td className="tdata">{data.phNumber}</td>
+                      <td className="tdata responsive-table">
+                        {data.birthday}
+                      </td>
+                      <td className="tdata responsive-table">{data.gender}</td>
+                      <td className="tdata responsive-table">
+                        {data.nrc_num}/{data.nrc_state}
+                        {data.nrc_national}/{data.nrc_numbers}
+                      </td>
+                      <td className="tdata responsive_h">
+                        {data.time} {data.hour} H
+                      </td>
+                      <HandleManipulation
+                        data={data}
+                        handleDelete={handleDelete}
+                      />
+                    </tr>
+                  );
+                })}
+            </tbody>
+          </table>
+          <div className="second-flex">
+            <p className="except">Showing 1 to {length} entries</p>
+            <ReactPaginate
+              containerClassName="btn-box"
+              activeLinkClassName="active"
+              breakLabel="..."
+              nextLabel="next >"
+              onPageChange={handlePageClick}
+              pageRangeDisplayed={5}
+              pageCount={pageCount}
+              previousLabel="< previous"
+              renderOnZeroPageCount={null}
+            />
+          </div>
         </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default ChildUserTable
+export default ChildUserTable;
